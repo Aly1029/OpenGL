13aafa4615ffe751fa5b27871de6f700d1471f6f
Aly1029 <femida1029@rambler.ru>
2016-11-10 21:40:27 +0300

    Opengl_fig
    new fog
    Teapot
    Icosahedron

--- OpenGL-1/OpenGL-1/HW.cpp
+++ OpenGL-1/OpenGL-1/HW.cpp
@@ -10,33 +10,56 @@ using namespace std;
 void changeViewPort(int w, int h)
 {
 	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, w / h, 0.1, 10);
+	gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
+	glMatrixMode(GL_MODELVIEW);
 }
 
 void render()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glutSolidTeapot(1.0);
+	//glutSolidTeapot(1.0);// чайник
+	
+	//glutSolidCube(1.0);
+	//glutSolidDodecahedron();
+	//glutSolidOctahedron();	
+	glutWireIcosahedron();	
+	glClearColor(0.3, 0.2, 0.6, 1.0);
+	glColor3f(0.0, 0.0, 1.0);
+
+
+	glPushMatrix();
+	glTranslatef(-0.6, 0, 0);
+	glutSolidTeapot(0.15);
+	glTranslatef(-0.6, 0, 0);
+	glutSolidTeapot(0.17);
+	glTranslatef(-0.6, 0, 0);
+	glRotatef(90, 0, 1, 0);
+	glutWireTeapot(0.19);
+	glPopMatrix();
+
+	
+
+
 	glutSwapBuffers();
 }
 
 
 
+
 int main(int argc, char* argv[]) {
 
-	// Initialize GLUT
-	glutInit(&argc, argv);
-	// Set up some memory buffers for our display
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-	// Set the window size
-	glutInitWindowSize(800, 600);
-	// Create the window with the title "Hello,GL"
-	glutCreateWindow("Hello, GL");
-	// Bind the two functions (above) to respond when necessary
-	glutReshapeFunc(changeViewPort);
-	glutDisplayFunc(render);
+		glutInit(&argc, argv);	
+		glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+		glutInitWindowSize(800, 600);
+		glutCreateWindow("Hello, GL");
+		glutReshapeFunc(changeViewPort);
+		glutDisplayFunc(render);
+		
 
-	// Very important!  This initializes the entry points in the OpenGL driver so we can 
-	// call all the functions in the API.
+	
 	GLenum err = glewInit();
 	if (GLEW_OK != err) {
 		fprintf(stderr, "GLEW error");
